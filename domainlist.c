1ea8d30338b5225324ade4cb2089f2c39f81cd97
holmium <3548216@gmail.com>
2013-07-06 12:25:02 +0800

    Add dedicated data structure for excluded & disabled domain lists

---
+++ domainlist.c
@@ -0,0 +1,119 @@
+#include "domainlist.h"
+#include "utils.h"
+
+typedef struct _EntryForDomain{
+	_32BIT_INT	Offset;
+} EntryForDomain;
+
+int DomainList_Init(DomainList *dl, int InitialCount /* For no-wildcard domain */)
+{
+	if( StringList_Init(&(dl -> List), NULL, 0) != 0 )
+	{
+		return -1;
+	}
+
+	if( HashTable_Init(&(dl -> List_Pos), sizeof(EntryForDomain), InitialCount) != 0 )
+	{
+		StringList_Free(&(dl -> List));
+		return -2;
+	}
+
+	if( StringList_Init(&(dl -> List_W), NULL, 0) != 0 )
+	{
+		StringList_Free(&(dl -> List));
+		HashTable_Free(&(dl -> List_Pos));
+		return -3;
+	}
+
+	if( Array_Init(&(dl -> List_W_Pos), sizeof(EntryForDomain), 0, FALSE, NULL) != 0 )
+	{
+		StringList_Free(&(dl -> List));
+		HashTable_Free(&(dl -> List_Pos));
+		StringList_Free(&(dl -> List_W));
+		return -4;
+	}
+
+	return 0;
+}
+
+int DomainList_Add(DomainList *dl, const char *Domain)
+{
+	EntryForDomain NewEntry;
+
+	if( ContainWildCard(Domain) )
+	{
+		NewEntry.Offset = StringList_Add(&(dl -> List_W), Domain);
+
+		if( NewEntry.Offset >= 0 )
+		{
+			Array_PushBack(&(dl -> List_W_Pos), &NewEntry, NULL);
+			return 0;
+		} else {
+			return -1;
+		}
+	} else {
+		NewEntry.Offset = StringList_Add(&(dl -> List), Domain);
+
+		if( NewEntry.Offset >= 0 )
+		{
+			HashTable_Add(&(dl -> List_Pos), Domain, &NewEntry);
+			return 0;
+		} else {
+			return -2;
+		}
+	}
+
+}
+
+BOOL DomainList_Match(DomainList *dl, const char *Str)
+{
+	EntryForDomain *FoundEntry;
+
+	const char *FoundString;
+
+	/* First try to match without wildcards */
+	{
+		FoundEntry = HashTable_Get(&(dl -> List_Pos), Str, NULL);
+		while( FoundEntry != NULL )
+		{
+			FoundString = StringList_GetByOffset(&(dl -> List), FoundEntry -> Offset);
+			if( strcmp(FoundString, Str) == 0 )
+			{
+				return TRUE;
+			}
+
+			FoundEntry = HashTable_Get(&(dl -> List_Pos), Str, FoundEntry);
+		}
+
+	}
+
+	/* Second try to match with wildcard */
+	{
+		int loop;
+
+		for( loop = 0; loop != Array_GetUsed(&(dl -> List_W_Pos)); ++loop )
+		{
+			FoundEntry = (EntryForDomain *)Array_GetBySubscript(&(dl -> List_W_Pos), loop);
+			if( FoundEntry != NULL )
+			{
+				FoundString = StringList_GetByOffset(&(dl -> List_W), FoundEntry -> Offset);
+				if( WILDCARD_MATCH(FoundString, Str) == WILDCARD_MATCHED )
+				{
+					return TRUE;
+				}
+
+			} else {
+				return FALSE;
+			}
+		}
+		return FALSE;
+	}
+}
+
+void DomainList_Free(DomainList *dl)
+{
+	StringList_Free(&(dl -> List));
+	HashTable_Free(&(dl -> List_Pos));
+	StringList_Free(&(dl -> List_W));
+	Array_Free(&(dl -> List_W_Pos));
+}
---
+++ domainlist.h
@@ -0,0 +1,32 @@
+#ifndef DOMAINLIST_H_INCLUDED
+#define DOMAINLIST_H_INCLUDED
+
+#include "hashtable.h"
+#include "stringlist.h"
+#include "array.h"
+
+typedef struct _DomainList{
+	/* Domains without wildcards */
+	StringList	List;
+
+	/* Positions of every domain in `List', offsets */
+	HashTable	List_Pos;
+
+
+	/* Domains containing wildcards */
+	StringList	List_W;
+
+	/* Positions of every domain in `List_W', offsets */
+	Array		List_W_Pos;
+
+} DomainList;
+
+int DomainList_Init(DomainList *dl, int InitialCount /* For no-wildcard domain */);
+
+int DomainList_Add(DomainList *dl, const char *Domain);
+
+BOOL DomainList_Match(DomainList *dl, const char *Str);
+
+void DomainList_Free(DomainList *dl);
+
+#endif // DOMAINLIST_H_INCLUDED
